be82f02e9c6e02e1f9580d8b88c7d1375c0563f7
Tim-PC <tim.dahare@gmail.com>
2017-11-01 00:07:02 +0100

    Added simple playercontroller and test environment

---
+++ beginner-game/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+    CharacterController controller;
+    PlayerCamera camera;
+
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start () {
+        controller = GetComponent<CharacterController>();
+        camera = GetComponentInChildren<PlayerCamera>();
+    }
+
+	void Update () {
+        
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), -0.75f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0, camera.transform.rotation.eulerAngles.y, 0);
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ beginner-game/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour {
+
+    private const float CAM_MAX_ANGLE = 50f;
+    private float rotationX;
+    private float rotationY;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        rotationX -= Input.GetAxis("Mouse Y");
+        rotationY += Input.GetAxis("Mouse X");
+
+        rotationX = Mathf.Clamp(rotationX, -CAM_MAX_ANGLE, CAM_MAX_ANGLE);
+
+        Vector3 rotation = new Vector3(rotationX, rotationY, 0);
+
+        transform.rotation = Quaternion.Euler(rotation);
+    }
+}
